b8df8529e6fff2f41d0af1bff3d104a120e17fac
Scott McCool <scott.mccool@pubnub.com>
2020-06-18 23:47:29 -0700

    identify fuji tags and start buliding a packet

--- creyon-zombie/hub/fuji_tag_scanner.go
+++ creyon-zombie/hub/fuji_tag_scanner.go
@@ -43,29 +43,54 @@ func onStateChanged(d gatt.Device, s gatt.State) {
 }
 
 func onPeripheralDiscovered(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
-	//	reading, err := makeFujiTag()
-	fmt.Println("------------------------------")
-	printAdData(p, a, rssi)
-	//	reading, err := makeFujiTag(p, a, rssi)
-	//	if err != nil {
-	//		fmt.Println("Not a fujitsu tag")
-	//	} else {
-	//		fmt.Println("Found a fuji tag reading: ", reading)
-	//	}
+	reading, err := makeFujiTag(p, a, rssi)
+	if err != nil {
+		fmt.Println("ID: ", p.ID(), "Error: ", err)
+		//		fmt.Printf("ID: %s __not a fuji tag__\n", p.ID())
+	} else {
+		fmt.Println("***  Found Fuji Tag ***")
+		fmt.Printf("%+v \n", reading)
+		fmt.Println("***************")
+		//		rawc <- reading
+	}
 }
 
-//type Advertisement struct {
-//LocalName        string
-//ManufacturerData []byte
-//ServiceData      []ServiceData
-//Services         []UUID
-//OverflowService  []UUID
-//TxPowerLevel     int
-//Connectable      bool
-//SolicitedService []UUID
+// FBeacon - a Fujitsu tag
+type FBeacon struct {
+	hexString       string
+	tagId           string
+	rawData         []byte
+	readingsOnlyHex string
+	//uuid  string
+	//major uint16
+	//minor uint16
+}
 
+// makeFujiTag
+// A fujitsu tag packet as read by gatt looks like this:
+//p.ID(): E2:94:B4:AF:93:13
+//p.Name():
+//a.LocalName:
+// a.TxPowerLevel: 0
+// hex.EncodeToString(a.ManufacturerData): 590001000300030034065bfbb1febb06
+// a.ServiceData: []
+func makeFujiTag(p gatt.Peripheral, a *gatt.Advertisement, rssi int) (FBeacon, error) {
+	var reading FBeacon
+	vendorString, _ := hex.DecodeString("5900010003000300") // MfrData starts with this for our tags
+	if len(vendorString) <= len(a.ManufacturerData) {
+		if bytes.Equal(vendorString, a.ManufacturerData[0:len(vendorString)]) {
+			reading.rawData = a.ManufacturerData
+			reading.hexString = hex.EncodeToString(a.ManufacturerData)
+			reading.tagId = p.ID()
+			reading.readingsOnlyHex = hex.EncodeToString(a.ManufacturerData[len(a.ManufacturerData)-len(vendorString):])
+			return reading, nil
+		}
+	}
+	return reading, errors.New("Not a fuji tag")
+}
+
+// Used for reverse engineering ble ads
 func printAdData(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
-
 	var flipped []byte
 	vendorStringBytes := []byte("10003000300")
 	fmt.Printf("\nPeripheral ID:%s, NAME:(%s)\n", p.ID(), p.Name())
@@ -87,33 +112,4 @@ func printAdData(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
 		fmt.Println("** End Fuji details **")
 	}
 	fmt.Println("  Service Data      =", a.ServiceData)
-}
-
-// FBeacon - a Fujitsu tag
-type FBeacon struct {
-	rawData string
-	flipped string
-	//uuid  string
-	//major uint16
-	//minor uint16
-}
-
-// makeFujiTag
-// A fujitsu tag packet as read by gatt looks like this:
-//p.ID(): E2:94:B4:AF:93:13
-//p.Name():
-//a.LocalName:
-// a.TxPowerLevel: 0
-// hex.EncodeToString(a.ManufacturerData): 590001000300030034065bfbb1febb06
-//                                           59000100030003007F03A503C4FFA907
-//    (Flipped:                            a6fffefffcfffcffcbf9a4044e0144f9)
-// a.ServiceData: []
-func makeFujiTag(p gatt.Peripheral, a *gatt.Advertisement, rssi int) (FBeacon, error) {
-	var reading FBeacon
-	vendorStringBytes := []byte("5900010003000300")
-	if bytes.Equal(vendorStringBytes, a.ManufacturerData[0:len(vendorStringBytes)]) {
-		reading.rawData = hex.EncodeToString(a.ManufacturerData)
-		return reading, nil
-	}
-	return reading, errors.New("not a Fujitsu tag")
 }
