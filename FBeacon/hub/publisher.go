39406dac99def957ddd87ad31b793bf55a62774a
Scott McCool <scott.mccool@pubnub.com>
2020-06-20 22:38:53 -0700

    Revert "Revert "checkpoint""
    This reverts commit 5c9b5510f0c2b85ad817bc64a185f44f1fa4b6a5.

--- FBeacon/hub/publisher.go
+++ FBeacon/hub/publisher.go
@@ -5,22 +5,44 @@ package hub
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/scottmccool/FBeacon/gcloud"
+	"github.com/scottmccool/FBeacon/readings"
+
 )
 
 // Publish Reads from chan Analyzedc and publishes off hub (well, to stdout)
-func Publish() {
-	for {
+func Publish() (published int, err error) {
+	if len(Analyzedc) < pubMinBatchSize {
+		return 0, nil
+	}
+	var observations []readings.FBeacon
+	for len(observations) < pubMinBatchSize {
 		select {
 		case obs := <-Analyzedc:
-			//			fmt.Println(obs)
-			j, err := json.Marshal(obs)
-			if err != nil {
-				fmt.Println("Cannot publish:", obs)
-			} else {
-				fmt.Println(string(j))
-			}
-		default:
-			return
+			observations = append(observations, obs)
 		}
 	}
+
+	gcloud_err := gcloud.PublishToGcloud(observations)
+	stdout_err := publishToStdOut(observations)
+
+	if gcloud_err != nil {
+		return len(observations), gcloud_err
+	}
+	if stdout_err != nil {
+		return len(observations), stdout_err
+	}
+	return len(observations), nil
+}
+
+func publishToStdOut(observations []readings.FBeacon) error {
+	//	fmt.Println(observations)
+	for o := range observations {
+		s, err := json.Marshal(observations[o])
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(s))
+	}
+	return nil
 }
